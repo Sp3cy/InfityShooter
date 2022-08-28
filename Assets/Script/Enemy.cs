7a4494ee65b7b15400814cb9027ff7d2bf062110
Sasso <ricciuti.alessio@hotmail.com>
2022-08-28 20:08:13 +0200

    Enemy Improve

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [Header("- Enemy Stats")]
     public float life = 10f; 
     public float speed = 1f;
 
+    [Space(1)]
+    [Header("- Rotation Time")]
+    public float lerpT = 0.5f;
+
     private GameObject player;
+    private Rigidbody2D rbEnemy;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        rbEnemy = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -38,6 +45,14 @@ public class Enemy : MonoBehaviour
 
     private void MoveToPlayer()
     {
+        Vector2 lookDir;
+
+        // Sposta i nemici verso il player
         transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+
+        // Gira il rigidbody dei nemici
+        lookDir = player.transform.position - transform.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rbEnemy.rotation = Mathf.LerpAngle(rbEnemy.rotation, angle, lerpT);
     }
 }
--- Assets/Script/EnemySpawner.cs
+++ Assets/Script/EnemySpawner.cs
@@ -6,18 +6,25 @@ public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
 
-    public float rangeX;
-    public float rangeY;
+    public float minRangeX;
+    public float minRangeY;
+
+    public float maxRangeX;
+    public float maxRangeY;
 
     public float spawnDelay = 2f;
 
+    private Vector2 playerPos;
     private GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        StartCoroutine(SpawnWithDealy(enemyPrefab, spawnDelay));
+        StartCoroutine(EntitySpawner(enemyPrefab, spawnDelay));
+
+        playerPos = player.transform.position;
+        float angle = Mathf.Atan2(playerPos.y, playerPos.x) * Mathf.Rad2Deg - 90f;
     }
 
     // Update is called once per frame
@@ -26,16 +33,25 @@ public class EnemySpawner : MonoBehaviour
         
     }
 
-    private IEnumerator SpawnWithDealy(GameObject enemy, float delay)
+    private IEnumerator EntitySpawner(GameObject enemy, float delay)
     {
         yield return new WaitForSeconds(delay);
 
+        // Posizione player attuale
         float posX = player.transform.position.x;
         float posY = player.transform.position.y;
+
+        // Random tra distanza minima dal player e massima
+        float enemyPosX = Random.Range(minRangeX, maxRangeX);
+        float enemyPosY = Random.Range(minRangeY, maxRangeY);
+
+        // Random.Range(0, 2) * 2 - 1 == -1 or 1
+        Vector3 pos = new Vector2(posX + (Random.Range(0, 2) * 2 - 1) * enemyPosX, posY + (Random.Range(0, 2) * 2 - 1)*enemyPosY);
         
-        Vector3 pos = new Vector2(Random.Range(posX - rangeX, posX + rangeX), Random.Range(posY - rangeY, posY + rangeY));
+        // Non so se serva più di tanto creare un gameobject
         GameObject newEnemy = Instantiate(enemy, pos, Quaternion.identity);
 
-        StartCoroutine(SpawnWithDealy(enemy, delay));
+        // Restart this coroutine
+        StartCoroutine(EntitySpawner(enemy, delay));
     }
 }
