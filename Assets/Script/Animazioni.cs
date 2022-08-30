b15035053b69e3b843b1774f7fd3892dced32400
giuseppe borromeo <giuseppegiuseppe315@gmail.com>
2022-08-30 21:19:54 +0200

    LeanTween

---
+++ Assets/Script/Animazioni.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animazioni : MonoBehaviour
+{
+
+    public void BounceText(float potenzaAnimazione, float durataAnimazione, GameObject oggetto) //Animazione bouncing.
+    {
+        LeanTween.cancel(oggetto);
+
+        oggetto.gameObject.transform.localScale = new Vector3(1, 1, 1);
+
+        LeanTween.scale(oggetto, Vector3.one * potenzaAnimazione, durataAnimazione).setEasePunch();
+    }
+
+}
