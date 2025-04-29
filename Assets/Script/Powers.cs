0ddfaf91c64557df4eab0634f95d2814c3cf67e3
giuseppe borromeo <giuseppegiuseppe315@gmail.com>
2025-04-29 23:40:05 +0200

    power up fixes
    grenade now go only forward and kunai have all the same force when launched

--- Assets/Script/Powers.cs
+++ Assets/Script/Powers.cs
@@ -127,7 +127,15 @@ public class Powers : MonoBehaviour
 
         grenadeCol.enabled = false;
 
-        grenadeRb.AddForce(new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * grenadeForce, ForceMode2D.Force);
+        // Direzione in avanti rispetto alla rotazione del player
+        Vector2 forwardDir = player.transform.up;
+
+        // Aggiungi una variazione angolare casuale fino a ±30 gradi
+        float angleOffset = Random.Range(-30f, 30f);
+        Vector2 randomizedDir = Quaternion.Euler(0, 0, angleOffset) * forwardDir;
+
+        // Applica la forza
+        grenadeRb.AddForce(randomizedDir.normalized * grenadeForce, ForceMode2D.Force);
 
         // Wait for grenade timer
         yield return new WaitForSeconds(grenadeTimer);
@@ -138,6 +146,7 @@ public class Powers : MonoBehaviour
         // Wait fix for collider function
         yield return new WaitForFixedUpdate();
         grenadeCol.enabled = false;
+
         var grenadeExplodeEffect = Instantiate(grenadeParticleExplosion, grenade.transform.position, Quaternion.identity);
 
         grenadeExplosionSound.pitch = Random.Range(1f, 1.3f);
@@ -150,6 +159,7 @@ public class Powers : MonoBehaviour
 
         yield return null;
     }
+
 
     protected IEnumerator Bolts(Transform enemyPos)
     {
@@ -181,7 +191,8 @@ public class Powers : MonoBehaviour
         var kunai = Instantiate(kunaiPrefab, player.transform.position, player.transform.rotation);
         Rigidbody2D kunaiRb = kunai.GetComponent<Rigidbody2D>();
         Collider2D kunaiCol = kunai.GetComponent<Collider2D>();
-        kunaiRb.AddForce(new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * grenadeForce, ForceMode2D.Force);
+        Vector2 randomDir = Random.insideUnitCircle.normalized;
+        kunaiRb.AddForce(randomDir * grenadeForce, ForceMode2D.Force);
 
         //wait destroy time
         yield return new WaitForSeconds(kunayDestroyTime);
