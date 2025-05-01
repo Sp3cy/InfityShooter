b01dab092463c506fdce8bed83c06c56c1c412bc
Sasso <ricciuti.alessio@hotmail.com>
2025-05-01 17:17:13 +0200

    Nothing

--- Assets/Script/NewSystem/HealthSystem.cs
+++ Assets/Script/NewSystem/HealthSystem.cs
@@ -1,24 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class HealthSystem : MonoBehaviour
 {
-    [field:SerializeField] public float HpBase { get; private set; }
-    private float HpActual;
+    [field:SerializeField] public float HP_BASE { get; private set; }
+    private float _hpActual;
+
+    public UnityEvent<float> OnCure;
+    public UnityEvent<float> OnDamage;
+
+    private void OnEnable()
+    {
+        OnCure.AddListener(HpAdd);
+        OnDamage.AddListener(HpRemove);
+    }
+
+    private void OnDisable()
+    {
+        OnCure.RemoveListener(HpAdd);
+        OnDamage.RemoveListener(HpRemove);
+    }
 
     private void Awake()
     {
-        if (!(HpBase > 0))
+        if (!(HP_BASE > 0))
         {
             Debug.LogError("ERROR HEALTH_SYSTEM: " + this.gameObject + " - HpBase is not valid, passing base value of 100!");
         }
 
-        HpActual = (HpBase > 0) ? HpBase : 100f;
+        _hpActual = (HP_BASE > 0) ? HP_BASE : 100f;
     }
 
-    private void RemoveHealth(float amount)
+    private void HpAdd(float amount)
     {
 
+    }
+
+    private void HpRemove(float amount)
+    {
+        _hpActual -= amount;
+
+        if (!(_hpActual > 0))
+        {
+            
+        }
     }
 }
